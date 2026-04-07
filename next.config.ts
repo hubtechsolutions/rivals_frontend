52aee6fa08a75abf19033d45f4c4393762670759
Kanak <kanakkumarmahala000@gmail.com>
2026-04-07 14:54:30 +0530

    fix : social post sectoin

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,30 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "ferf1mheo22r9ira.public.blob.vercel-storage.com",
       },
+      {
+        protocol: "https",
+        hostname: "**.fbcdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "**.cdninstagram.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.licdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.twimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
+      },
     ],
   },
 };
